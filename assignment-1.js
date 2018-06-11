8b9193aea46225d945485c5255c7fa736e320f8a
Adam Capasso <adamjcapasso@gmail.com>
2018-06-11 09:09:36 -0400

    First Commit

---
+++ assignment-1.js
@@ -0,0 +1,37 @@
+/* 
+My notes on this assignment:
+I got a little stuck at the array push() method part, but 
+eventually entered each integer in on a separate line (as seen below).
+I later tested it with just "numbers.push (i);" and got the same result.
+*/
+
+
+var numbers = [];                     
+
+for (var i=0; i < 10; i++){           
+    numbers[0] = 0;                   
+    numbers[1] = 1;                   
+    numbers[2] = 2;
+    numbers[3] = 3;
+    numbers[4] = 4;
+    numbers[5] = 5;
+    numbers[6] = 6;
+    numbers[7] = 7;
+    numbers[8] = 8;
+    numbers[9] = 9;
+}
+
+console.log (numbers); 
+console.log(numbers [0]);
+console.log(numbers [9]);
+
+
+var car = {
+    colour: "blank"
+}
+
+
+
+ 
+
+
